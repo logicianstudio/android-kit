ff6f40fdb0b1fa347a394a79778f6ef55f2e2e05
Mustahsan <mustahsan.junaid@gmail.com>
2022-04-30 03:12:19 +0500

    minor updates

--- kit/src/main/java/com/android/kit/Logs.kt
+++ kit/src/main/java/com/android/kit/Logs.kt
@@ -3,58 +3,67 @@ package com.android.kit
 import android.util.Log
 import java.lang.Exception
 
-fun Any.logV(log: String) {
-    logV(this::class.java.simpleName, log)
+fun Any.logV(message: String) {
+    logV(this::class.java.simpleName, message)
 }
 
-fun Any.logE(log: String) {
-    logE(this::class.java.simpleName, log)
+fun Any.logE(message: String) {
+    logE(this::class.java.simpleName, message)
 }
 
-fun Any.logD(log: String) {
-    logD(this::class.java.simpleName, log)
+fun Any.logD(message: String) {
+    logD(this::class.java.simpleName, message)
 }
 
-fun Any.logI(log: String) {
-    logI(this::class.java.simpleName, log)
+fun Any.logI(message: String) {
+    logI(this::class.java.simpleName, message)
 }
 
-fun Any.logWtf(log: String) {
-    logWtf(this::class.java.simpleName, log)
+fun Any.logWtf(message: String) {
+    logWtf(this::class.java.simpleName, message)
 }
 
-fun Any.logW(log: String) {
-    Log.w(this::class.java.simpleName, log)
+fun Any.logW(message: String) {
+    Log.w(this::class.java.simpleName, message)
 }
 
-fun Any.logV(tag: String, log: String) {
-    Log.v(tag, log)
+fun Any.logV(tag: String, message: String) {
+    Log.v(tag, message)
 }
 
-fun Any.logE(log: String, e: Exception?) {
-    logE(this::class.java.simpleName, log, e)
+fun Any.logE(message: String, e: Exception?) {
+    logE(this::class.java.simpleName, message, e)
 }
 
-fun Any.logE(tag: String, log: String) {
-    Log.e(tag, log)
+fun Any.logE(tag: String, message: String) {
+    Log.e(tag, message)
 }
 
-fun Any.logE(tag: String, log: String, e: Exception?) {
-    Log.e(tag, log, e)
+fun Any.logE(tag: String, message: String, e: Exception?) {
+    Log.e(tag, message, e)
 }
 
-fun Any.logD(tag: String, log: String) {
-    Log.d(tag, log)
+fun Any.logD(tag: String, message: String) {
+    Log.d(tag, message)
 }
 
-fun Any.logI(tag: String, log: String) {
-    Log.i(tag, log)
+fun Any.logI(tag: String, message: String) {
+    Log.i(tag, message)
 }
 
-fun Any.logW(tag: String, log: String) {
-    Log.w(tag, log)
+fun Any.logW(tag: String, message: String) {
+    Log.w(tag, message)
 }
 
-fun Any.logWtf(tag: String, log: String) {
-    Log.wtf(tag, log)
+fun Any.logWtf(tag: String, message: String) {
+    Log.wtf(tag, message)
+}
+
+fun Any.logMethodName(message: String) {
+    val methodName = if (Thread.currentThread().stackTrace.size > 3) {
+        Thread.currentThread().stackTrace[3].methodName
+    } else {
+        ""
+    }
+    logI("$methodName : $message")
 }
--- kit/src/main/java/com/android/kit/ktx/Any.kt
+++ kit/src/main/java/com/android/kit/ktx/Any.kt
@@ -1,3 +1,10 @@
 package com.android.kit.ktx
 
+import android.os.Handler
+import android.os.Looper
+
 fun Any.throwException(message: String): Nothing = throw Exception("${javaClass.simpleName}: $message")
+
+fun Any.postDelayed(delay: Long, task: () -> Unit) {
+    Handler(Looper.getMainLooper()).postDelayed(task, delay)
+}
